a428dd10e3d746cbccc13282dafd1764b15ef07b
sushanth3104 <sushkumarthaviti@gmail.com>
2025-02-26 16:08:41 +0530

    ADD INSTRUCTION REGISTER

---
+++ DataPath/InstructionMemory/InstructionMemory.v
@@ -0,0 +1,15 @@
+module InstructionMemory (
+    input [31:0] readAddr,
+    output [31:0] inst
+);
+    
+    
+    // For simplicty taking smaller memory
+
+    reg [7:0] insts [127:0];
+    
+    assign inst = (readAddr >= 128) ? 32'b0 : {insts[readAddr+3], insts[readAddr + 2], insts[readAddr + 1], insts[readAddr]};
+
+
+endmodule
+
---
+++ DataPath/InstructionMemory/tb.v
@@ -0,0 +1,39 @@
+`timescale 1ns/1ps
+`include "InstructionMemory.v"
+
+module tb;
+
+parameter WIDTH = 32;
+
+reg [WIDTH-1:0] readAddr;
+wire [WIDTH-1:0] inst;
+
+InstructionMemory im (
+    .readAddr(readAddr),
+    .inst(inst)
+);
+
+initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    $readmemb("TEST_INSTRUCTIONS.dat", im.insts,0,127);
+    readAddr = 0;
+    #10;
+    readAddr = 4;
+    #10;
+    readAddr = 8;
+    #10;
+    readAddr = 12;
+    #10;
+    readAddr = 16;
+    #10;
+    readAddr = 20;
+    #10;    
+    readAddr = 24;
+    #10;
+    $finish;
+
+end
+
+
+endmodule
